6b2cc2479204908a6e7abdf3550b007c0fde6bcf
gjholman <gholman@wpi.edu>
2018-11-10 19:58:13 -0500

    Remove unnecessary files and add helper libraries

---
+++ _algorithms.py
@@ -0,0 +1,59 @@
+# This is a collection of core algorithms to help with learning about
+# algorithms while working through problems in cracking the coding interview.
+# Each algorithm will be written and tested and will include an explaination
+# of the space and time complexity
+
+# Fibbonachi (Fibbonachi? Fibo-gnokki) sequence
+def fib(n):
+    a, b = 0, 1
+    while b < n:
+        print b,
+        a, b = b, a+b
+
+def linearSearch(vector):
+    return 0
+
+def binarySearch(vector):
+    return 0
+
+def bubbleSort(vector):
+    return 2
+
+def selectionSort(vector):
+    return 2
+
+def insertionSort(vector):
+    return 2
+
+def heapSort(vector):
+    return "Columbus, Ohio"
+
+def mergeSort(vector):
+    return 2
+
+def quickSort(vector):
+    return 0
+
+def breadthFirstSearch(vector):
+    return 0
+
+def depthSearchFirst(vector):
+    return 0
+
+def bellmanFordAlgorithm(vector):
+    return 0
+
+def dijkstrasAlgorithm(vector):
+    return 0
+
+def euclidianAlgorithm(vector):
+    return 0
+
+def primalityTest(vector):
+    return 0
+
+def runLengthEncoding(vector):
+    return 5
+
+def huffmanEncoding(vector):
+    return 5
